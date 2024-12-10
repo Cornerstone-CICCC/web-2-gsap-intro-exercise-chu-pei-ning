7ffbf457080f7ceda26f22b544982bd3f37d0393
Julia_Chu <z3211232001@gmail.com>
2024-12-10 12:31:22 -0800

    finished

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // PUT YOUR CODE HERE
+
+const boxTimeline = gsap.timeline()
+
+boxTimeline
+  .from('.red', { x: '-100vw', y: '-100vh', duration: .8})
+  .from('.green', { x: '100vw', y: '-100vh', duration: .8 })
+  .from('.blue', { x: '100vw', y: '100vh', duration: .8 })
+  .from('.yellow', { x: '-100vw', y: '100vh', duration: .8 })
+
+  .to('.red', { x: '-100%', left: '100%', borderRadius: '50%', backgroundColor: 'green', duration: .8 })
+  .to('.green', { y: '-100%', top:'100%', borderRadius: '50%', backgroundColor: 'blue', duration: .8 },'<')
+  .to('.blue', { x: 0, left: 0, borderRadius: '50%', backgroundColor: 'yellow', duration: .8 },'<')
+  .to('.yellow', { y: 0, top: 0, borderRadius: '50%', backgroundColor: 'red', duration: .8 },'<')
+  
+  .to('.yellow', { x: '-100%', left:0, duration: 1})
+  .to('.red', { left: 'calc(100vw + 100%)', duration: 1}, '<')
+  .to('.green', { x: '100%', left:'100%', duration: 1},'<')
+  .to('.blue', { left: 'calc(-100vw - 100%)', duration: 1},'<')
+
